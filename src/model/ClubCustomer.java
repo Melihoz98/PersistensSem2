0ea4518ac0214d5b82b451f2ecb62cf6c7a6e34b
Ahmad <10478728@ucn.dk>
2023-10-10 14:47:32 +0200

    ClubCustomer update

--- src/model/ClubCustomer.java
+++ src/model/ClubCustomer.java
@@ -1,5 +1,16 @@
 package model;
 
-public class ClubCustomer {
+public class ClubCustomer extends Customer{
+
+	private String clubType;
+	private String clubName;
+	
+	public ClubCustomer(int customerID, String lastName, String firstName, String streetName, String houseNo,
+			String zipCode, String customerType, String clubType, String clubName) {
+		super(customerID, lastName, firstName, streetName, houseNo, zipCode, customerType);
+		this.clubType = clubType;
+		this.clubName = clubName;
+		// TODO Auto-generated constructor stub
+	}
 
 }
