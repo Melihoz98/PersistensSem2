f6b143f6cc69b9c46aa0d34844193ad354a8a90a
Ahmad <10478728@ucn.dk>
2023-10-10 21:22:08 +0200

    Update Supplier

--- src/model/Supplier.java
+++ src/model/Supplier.java
@@ -6,8 +6,64 @@ public class Supplier {
 private int ID;
 private String name;
 private String streetName;
-private int HouseNo;
-private int ZipCode;
+private int houseNo;
+private int zipCode;
 
+
+
+public Supplier() {
+ 
+}
+
+
+public Supplier(int ID, String name, String streetName, int houseNo, int zipCode) {
+    this.ID = ID;
+    this.name = name;
+    this.streetName = streetName;
+    this.houseNo = houseNo;
+    this.zipCode = zipCode;
+}
+
+
+
+public int getID() {
+    return ID;
+}
+
+public void setID(int ID) {
+    this.ID = ID;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public String getStreetName() {
+    return streetName;
+}
+
+public void setStreetName(String streetName) {
+    this.streetName = streetName;
+}
+
+public int getHouseNo() {
+    return houseNo;
+}
+
+public void setHouseNo(int houseNo) {
+    this.houseNo = houseNo;
+}
+
+public int getZipCode() {
+    return zipCode;
+}
+
+public void setZipCode(int zipCode) {
+    this.zipCode = zipCode;
+}
 
 }
