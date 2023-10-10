ac8698eeb48fa5f5ecbbffc3e2a25cd2daa2cff2
SkoledrengQ <116273731+SkoledrengQ@users.noreply.github.com>
2023-10-10 14:17:18 +0200

    Added to 3 classes
    Changes to CustomerPhones, Phones & Product

--- src/model/CustomerPhones.java
+++ src/model/CustomerPhones.java
@@ -1,5 +1,28 @@
 package model;
 
-public class CustomerPhones {
+public class CustomerPhones extends Phones {
 
+    private String phoneID;
+    private String customerID;
+
+    public CustomerPhones(String phoneID, String customerID, String phoneNumber, String phoneType) {
+        this.phoneID = phoneID;
+        this.customerID = customerID;
+    }
+
+    public String getPhoneID() {
+        return phoneID;
+    }
+
+    public void setPhoneID(String phoneID) {
+        this.phoneID = phoneID;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
 }
