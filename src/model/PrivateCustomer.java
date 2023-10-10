263cb2c09fdcb7671261d09de5733b638d231619
Ahmad <10478728@ucn.dk>
2023-10-10 21:13:23 +0200

    Update PrivateCustomer

--- src/model/PrivateCustomer.java
+++ src/model/PrivateCustomer.java
@@ -1,5 +1,7 @@
 package model;
 
-public class PrivateCustomer {
-
+public class PrivateCustomer extends Customer {
+    public PrivateCustomer(int customerID, String lastName, String firstName, String streetName, String houseNo, String zipCode, String customerType) {
+        super(customerID, lastName, firstName, streetName, houseNo, zipCode, customerType);
+    }
 }
