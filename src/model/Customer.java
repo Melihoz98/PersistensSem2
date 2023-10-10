90afeaf4727c1907731aa12ea7190d8b8cc64cb1
Ahmad <10478728@ucn.dk>
2023-10-10 14:47:00 +0200

    Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,13 +1,85 @@
+
 package model;
 
 public class Customer {
 
-	private int customerID;
-	private String lastName;
-	private String firstName;
-	
-	
-	
-	
+    private int customerID;
+    private String lastName;
+    private String firstName;
+    private String streetName;
+    private String houseNo;
+    private String zipCode;
+    private String customerType;
 
+    
+
+    public Customer(int customerID, String lastName, String firstName, String streetName, String houseNo, String zipCode, String customerType) {
+        this.customerID = customerID;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.streetName = streetName;
+        this.houseNo = houseNo;
+        this.zipCode = zipCode;
+        this.customerType = customerType;
+       
+    }
+
+    // Getter methods
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public String getHouseNo() {
+        return houseNo;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+
+    // Setter methods
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setHouseNo(String houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
 }
