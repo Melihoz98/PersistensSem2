31864d5cb287f8b5384d0e1a651ffd2e541e2c25
Lasselarsen4 <10478730@ucn.dk>
2023-10-12 11:02:41 +0200

    Update testAddProduct

--- src/test/testAddProduct.java
+++ src/test/testAddProduct.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Equipment;
-import model.Order;
 import model.Product;
+import model.SaleOrder;
 
 class testAddProduct {
 	
 	private Product p;
-	private Order o;
+	private SaleOrder so;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		o = new Order();
+		so = new SaleOrder(0, false, 0, 0);
 		p = new Equipment(123, "Replica Revolver", "A replica of a Revolver", 14.99, "Equipment", "USA", 9.99, "Blank");
 		
 	}
@@ -28,26 +28,26 @@ class testAddProduct {
 
 	@Test
 	void testAddProductShouldComplete() {
-		o.addProduct(p, 3);
-		assertEquals(o.getProducts().get(0).getProduct(),p);
+		so.addProduct(p, 3);
+		assertEquals(so.getProducts().get(0).getProduct(),p);
 	}
 	
 	@Test
 	void testAddNullProductShouldFail() {
-		o.addProduct(null, 3);
-		assertEquals(o.getProducts().size(),0);
+		so.addProduct(null, 3);
+		assertEquals(so.getProducts().size(),0);
 	}
 	
 	@Test
 	void testAddProductAlreadyInTheOrderShouldComplete() {
-		o.addProduct(p, 3);
-		o.addProduct(p, 2);
-		assertEquals(o.getProducts().get(0).getQuantity(),5);
+		so.addProduct(p, 3);
+		so.addProduct(p, 2);
+		assertEquals(so.getProducts().get(0).getQuantity(),5);
 	}
 	@Test
 	void testAddProductZeroQuantityShouldFail() {
-		o.addProduct(p, 0);
-		assertEquals(o.getProducts().size(),0);
+		so.addProduct(p, 0);
+		assertEquals(so.getProducts().size(),0);
 	}
 
 }
